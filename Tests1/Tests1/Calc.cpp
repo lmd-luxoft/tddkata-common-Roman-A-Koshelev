05a818c5a5744436881cff876a69ea0a4a34f659
Roman Koshelev <RomanKoshelev@gmail.com>
2021-04-14 11:07:41 +0300

    Сделал реализацию задания 1, доработал и отладил unit-тесты

--- Tests1/Tests1/Calc.cpp
+++ Tests1/Tests1/Calc.cpp
@@ -1,5 +1,10 @@
 #include "pch.h"
 #include "Calc.h"
+#include <charconv>
+#include<string_view>
+
+static const int ERR_CODE_SUCCESS = 0;
+static const int ERR_CODE_FAILD = -1;
 
 Calc::Calc() {
 }
@@ -7,6 +12,75 @@ Calc::Calc() {
 Calc::~Calc() {
 }
 
-int Calc::sum(std::string expression) {
-	throw(0);
+int parse_str(
+		std::string_view str
+		, std::string_view& val1
+		, std::string_view& val2) {
+	static constexpr size_t comma_len = 1;
+	static constexpr char comma = ',';
+	size_t pos;
+	size_t val_count;
+
+	if (str.empty()) {
+		val_count = 0;
+		return val_count;
+	}
+
+	// Находим разделитель
+	pos = str.find(comma);
+	if (std::string_view::npos == pos) {
+		val_count = 1;
+		val1 = str;
+		return val_count;
+	}
+
+	val_count = 2;
+	val1 = str.substr(0, pos);
+	val2 = str.substr(pos + 1);
+	return val_count;
+}
+
+int Calc::sum(std::string expr) {
+	static constexpr char dot = '.';
+	std::string_view val1;
+	std::string_view val2;
+	std::int64_t v1 = 0;
+	std::int64_t v2 = 0;
+
+	int err_code = parse_str(expr, val1, val2);
+
+	int val_count = err_code;
+	if (0 < val_count) {
+		if (std::string_view::npos != val1.find(dot)) {
+			return ERR_CODE_FAILD;
+		}
+		if (auto [p, ec] = std::from_chars(val1.data(), val1.data() + val1.length(), v1);
+			ec != std::errc()) {
+			return ERR_CODE_FAILD;
+		}
+		if (v1 < 0) {
+			return ERR_CODE_FAILD;
+		}
+	}
+
+	if (1 < val_count) {
+		if (std::string_view::npos != val2.find(dot)) {
+			return ERR_CODE_FAILD;
+		}
+		if (auto [p, ec] = std::from_chars(val2.data(), val2.data() + val2.length(), v2);
+			ec != std::errc()) {
+			return ERR_CODE_FAILD;
+		}
+		if (v2 < 0) {
+			return ERR_CODE_FAILD;
+		}
+	}
+
+	if (std::numeric_limits<int>::max() < v1
+			|| std::numeric_limits<int>::max() < v2
+			|| std::numeric_limits<int>::max() < v1 + v2) {
+		return ERR_CODE_FAILD;
+	}
+
+	return (int)(v1 + v2);
 }
--- Tests1/Tests1/test.cpp
+++ Tests1/Tests1/test.cpp
@@ -2,6 +2,7 @@
 #include <gtest/gtest.h>
 #include "Calc.h"
 
+// Ноль параметров
 TEST(CalcTest, EmptyStringShouldBeZero)
 {
 	Calc calc;
@@ -9,7 +10,6 @@ TEST(CalcTest, EmptyStringShouldBeZero)
 	int actual = calc.sum("");
 	ASSERT_EQ(expected, actual);
 }
-
 
 TEST(CalcTest, Sum0ShouldBe0)
 {
@@ -35,6 +35,26 @@ TEST(CalcTest, Val75and125ShouldBe200)
 	ASSERT_EQ(expected, actual);
 }
 
+TEST(CalcTest, VeryBigVal_1)
+{
+	Calc calc;
+	int expected = std::numeric_limits<int>::max();
+	std::string expr1 = std::to_string(std::numeric_limits<int>::max() / 2 + 1);
+	std::string expr2 = std::to_string(std::numeric_limits<int>::max() / 2);
+	int actual = calc.sum((expr1 + "," + expr2).c_str());
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(CalcTest, VeryBigVal_2)
+{
+	Calc calc;
+	int expected = std::numeric_limits<int>::max();
+	std::string expr1 = std::to_string(std::numeric_limits<int>::max() / 2);
+	std::string expr2 = std::to_string(std::numeric_limits<int>::max() / 2 + 1);
+	int actual = calc.sum((expr1 + "," + expr2).c_str());
+	ASSERT_EQ(expected, actual);
+}
+
 TEST(CalcTest, AnyNegValIsError1)
 {
 	Calc calc;
@@ -48,6 +68,22 @@ TEST(CalcTest, AnyNegValIsError2)
 	Calc calc;
 	int expected = -1;
 	int actual = calc.sum("-1000000");
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(CalcTest, AnyNegValIsError3)
+{
+	Calc calc;
+	int expected = -1;
+	int actual = calc.sum("-10,10");
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(CalcTest, AnyNegValIsError4)
+{
+	Calc calc;
+	int expected = -1;
+	int actual = calc.sum("10,-10");
 	ASSERT_EQ(expected, actual);
 }
 
@@ -112,5 +148,43 @@ TEST(CalcTest, FloatingPointValIsError3)
 	Calc calc;
 	int expected = -1;
 	int actual = calc.sum("10.0,20.0");
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(CalcTest, VeryBigValIsError1)
+{
+	Calc calc;
+	int expected = -1;
+	std::string expr = std::to_string((std::int64_t)(std::numeric_limits<int>::max()) + 1);
+	int actual = calc.sum((expr+",10").c_str());
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(CalcTest, VeryBigValIsError2)
+{
+	Calc calc;
+	int expected = -1;
+	std::string expr = std::to_string((std::int64_t)(std::numeric_limits<int>::max()) + 1);
+	int actual = calc.sum(("10," + expr).c_str());
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(CalcTest, VeryBigValIsError3)
+{
+	Calc calc;
+	int expected = -1;
+	std::string expr1 = std::to_string(std::numeric_limits<int>::max()/2 + 2);
+	std::string expr2 = std::to_string(std::numeric_limits<int>::max()/2);
+	int actual = calc.sum((expr1 + "," + expr2).c_str());
+	ASSERT_EQ(expected, actual);
+}
+
+TEST(CalcTest, VeryBigValIsError4)
+{
+	Calc calc;
+	int expected = -1;
+	std::string expr1 = std::to_string(std::numeric_limits<int>::max() / 2);
+	std::string expr2 = std::to_string(std::numeric_limits<int>::max() / 2 + 2);
+	int actual = calc.sum((expr1 + "," + expr2).c_str());
 	ASSERT_EQ(expected, actual);
 }
